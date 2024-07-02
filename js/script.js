6a562cee7c490365081ac92926a2d0cf32116b71
raflynas <rafltnas99@gmail.com>
2024-07-02 18:52:51 +0700

    config submit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+function validateForm() {
+    const name = document.forms["message-form"]["full-name"].value;
+    const birthDate = document.forms["message-form"]["birth-date"].value;
+    const gender = document.forms["message-form"]["gender"].value;
+    const messages = document.forms["message-form"]["messages"].value;
+
+    if (name == "" || birthDate == "" || gender == "" || messages == "") {
+        alert("Tidak boleh ada yang kosong");
+    }
+
+    setSenderUI(name, birthDate, gender, messages);
+
+    return false;
+}
+
+function setSenderUI(name, birthDate, gender, messages) {
+    document.getElementById("sender-full-name").innerHTML = name;
+    document.getElementById("sender-birth-date").innerHTML = birthDate;
+    document.getElementById("sender-gender").innerHTML = gender;
+    document.getElementById("sender-messages").innerHTML = messages;
+}
+
+function replaceName() {
+    let name = prompt("Siapakah nama Anda?", "");
+    document.getElementById("namauser").innerHTML = name
+}
+
+replaceName();
+
+
